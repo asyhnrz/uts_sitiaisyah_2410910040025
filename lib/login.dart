98718b5b69a2c9d50444e96626987db3cbdadc10
asyhnrz <nurazkiyahaisyah02@gmail.com>
2025-10-23 19:38:12 +0700

    bagian1

---
+++ lib/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginSatu extends StatefulWidget {
+  const LoginSatu({super.key});
+
+  @override
+  State<LoginSatu> createState() => _LoginSatuState();
+}
+
+class _LoginSatuState extends State<LoginSatu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        
+      ),
+    );
+  }
+}

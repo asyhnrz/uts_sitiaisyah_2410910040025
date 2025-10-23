bb5cdbdd33e63ff36d5967e33a4b45e2f3f8cee2
asyhnrz <nurazkiyahaisyah02@gmail.com>
2025-10-23 20:56:39 +0700

    b5

--- lib/login.dart
+++ lib/login.dart
@@ -16,16 +16,28 @@ class _LoginState extends State<Login> {
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Padding(padding: const EdgeInsets.only(top: 300)),
             Image.asset("gambar/Logo.png"),
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("LKS"),
-                Text("MART"),
+                Text("LKS", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 80, 136, 233))),
+                Text("MART", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 80, 136, 233)),),
               ],
             ),
           ],
         ),
-        SizedBox(height: 20,)
+        SizedBox(height: 10,),
+
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text("Sign In", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 4, 6, 10))),
+            ],
+          ),
+        )
         ],
       ),
     );
